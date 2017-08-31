79affe2cd5a0f8eb0de9dafdbf917bb0b6aa5767
VasilevKA <VasilevKA@NEWVASILEVKA>
2017-08-31 20:11:32 +0300

    Timeouts for loading page

--- SeleniumTestApp/UnitTest1.cs
+++ SeleniumTestApp/UnitTest1.cs
@@ -11,21 +11,31 @@ namespace SeleniumTestApp
         [TestMethod]
         public void TestMethod1()
         {
-            IWebDriver driver = new ChromeDriver();
+            var loginText = "Пользователь 1";
+            var passwordText = "111";
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                 
+                driver.Navigate().GoToUrl("http://localhost:63171/Account/LogIn");
              
-            driver.Navigate().GoToUrl("localhost");
-             
-            IWebElement loginElement = driver.FindElement(By.Name("login"));
-            loginElement.SendKeys("Пользователь 1");
+                IWebElement loginElement = driver.FindElement(By.Name("UserName"));
+                loginElement.SendKeys(loginText);
 
-            IWebElement passElement = driver.FindElement(By.Name("pass"));
-            passElement.SendKeys("111");
+                IWebElement passElement = driver.FindElement(By.Name("Password"));
+                passElement.SendKeys(passwordText);
 
-            IWebElement sobmitElement = driver.FindElement(By.Name("submit"));
+                IWebElement sobmitElement = driver.FindElement(By.Name("submit"));
             
-            sobmitElement.Click();
+                sobmitElement.Click();
+
+                driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+            
+                IWebElement loginResult = driver.FindElement(By.ClassName("login"));
              
-            driver.Close();
+                Assert.IsTrue(loginResult.Text.Contains(loginText));
+            
+                driver.Close();
+            }
         }
     }
 }
